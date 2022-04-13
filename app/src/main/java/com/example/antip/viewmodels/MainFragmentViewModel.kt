99255c33db88b7c28fb4182801bb913ddb1edcec
not2exe <notexe2812@gmai.com>
2022-04-14 00:31:04 +0300

    half tuned MVVM

--- app/src/main/java/com/example/antip/viewmodels/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/antip/viewmodels/MainFragmentViewModel.kt
@@ -12,8 +12,7 @@ import com.google.android.material.internal.ContextUtils.getActivity
 
 class MainFragmentViewModel: ViewModel() {
     val currentAppsLD: MutableLiveData<List<App>> = MutableLiveData<List<App>>()
-    val app = App(getActivity(this).getDrawable(R.drawable.app1)!!,"1",123)
-    adapter.addApp(app)
+
     @RequiresApi(Build.VERSION_CODES.Q)
     fun onScreenStarted() {
     }
